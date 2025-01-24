9182e10cf77eb62e5ae7da25b721e67126906ad9
Thai Hoa Nguyen <tnguyen568@my.bcit.ca>
2025-01-24 11:40:37 -0800

    First commit

---
+++ Program.cs
@@ -0,0 +1,94 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Scalar.AspNetCore;
+using StudentsMinimalApi.Data;
+using StudentsMinimalApi.Models;
+using StudentsMinimalApi.Services;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+// Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
+builder.Services.AddOpenApi();
+
+var connStr = builder.Configuration.GetConnectionString("DefaultConnection") 
+    ?? throw new InvalidOperationException("Connection string 'DefaultConnection' not found.");
+builder.Services.AddDbContext<SchoolDbContext>(
+    option => option.UseSqlite(connStr));
+
+// Add Cors
+builder.Services.AddCors(o => o.AddPolicy("Policy", builder => {
+  builder.AllowAnyOrigin()
+    .AllowAnyMethod()
+    .AllowAnyHeader();
+}));
+
+//---------------------------------IMAGINARY LINE----------------------------------
+var app = builder.Build();
+app.UseCors("Policy");
+
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.MapOpenApi();
+    app.UseSwaggerUI(options => {
+
+        // // For our UI to be at ROOT
+        // options.RoutePrefix = "";
+
+        // options.SwaggerEndpoint("/openapi/v1.json", "My WebAPI");
+
+        // Scaler
+        app.MapScalarApiReference(options => {
+            options
+                .WithTitle("My WebAPI")
+                .WithTheme(ScalarTheme.Mars)
+                .WithDefaultHttpClient(ScalarTarget.CSharp, ScalarClient.HttpClient);
+        });
+    });
+}
+
+// var summaries = new[]
+// {
+//     "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
+// };
+
+// app.MapGet("/weatherforecast", () => 
+// {
+//     var forecast =  Enumerable.Range(1, 5).Select(index =>
+//         new WeatherForecast
+//         (
+//             DateOnly.FromDateTime(DateTime.Now.AddDays(index)),
+//             Random.Shared.Next(-20, 55),
+//             summaries[Random.Shared.Next(summaries.Length)]
+//         ))
+//         .ToArray();
+//     return forecast;
+// })
+// .WithName("GetWeatherForecast");
+
+
+var route = app.MapGroup("/api/students");
+
+route.MapGet("/", StudentService.GetAllStudents);
+route.MapGet("/school/{school}", StudentService.GetStudentsBySchool);
+route.MapGet("/{id}", StudentService.GetStudent);
+route.MapPost("/", StudentService.CreateSttudent);
+route.MapPut("/{id}", StudentService.UpdateStudent);
+route.MapDelete("/{id}", StudentService.DeleteStudent);
+
+
+using (var scope = app.Services.CreateScope()) {
+    var services = scope.ServiceProvider;
+
+    var context = services.GetRequiredService<SchoolDbContext>();    
+    context.Database.Migrate();
+}
+
+app.Run();
+
+// record WeatherForecast(DateOnly Date, int TemperatureC, string? Summary)
+// {
+//     public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
+// }
